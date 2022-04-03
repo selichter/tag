696ecefb1a671c3483a0e4dbc70312b7c6c26641
Slichter <lichter.sarahe@gmail.com>
2022-04-03 15:35:59 -0500

    Initial Commit

---
+++ Tag/TagApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagApp.swift
+//  Tag
+//
+//  Created by Sarah Lichter on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
